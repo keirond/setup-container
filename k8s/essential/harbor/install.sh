8ca321b7a833de7a511f460880bd37a56cf33b46
Bao Dang <dang.hbaokody@gmail.com>
2025-06-13 14:53:45 +0700

    up Fri Jun 13 07:53:45 AM UTC 2025

--- k8s/essential/harbor/install.sh
+++ k8s/essential/harbor/install.sh
@@ -7,9 +7,9 @@ SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 VALUE_FILE = "$SCRIPT_DIR/values.yaml"
 
 if [ -f "$VALUE_FILE" ]; then
-	helm install my-harbor bitnami/harbor --version 26.7.5 \
+	helm install harbor bitnami/harbor --version 26.7.5 \
 		-f "$SCRIPT_DIR/values.yaml" \
-		-n haproxy --create-namespace
+		-n registry --create-namespace
 else
 	echo "Not found: $VALUE_FILE"
 	exit 1
