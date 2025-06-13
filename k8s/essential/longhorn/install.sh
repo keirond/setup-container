11ecb55d2c8fb68773dca7b0bf9921afb2b55384
Bao Dang <dang.hbaokody@gmail.com>
2025-06-13 14:05:32 +0700

    up Fri Jun 13 07:05:32 AM UTC 2025

---
+++ k8s/essential/longhorn/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+VALUE_FILE = "$SCRIPT_DIR/values.yaml"
+
+if [ -f "$VALUE_FILE" ]; then
+	helm install haproxy bitnami/haproxy --version 2.2.24 \
+		-f "$SCRIPT_DIR/values.yaml" \
+		-n haproxy --create-namespace
+else
+	echo "Not found: $VALUE_FILE"
+	exit 1
+fi
---
+++ k8s/essential/longhorn/require.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+helm repo add longhorn https://charts.longhorn.io
+helm repo update
