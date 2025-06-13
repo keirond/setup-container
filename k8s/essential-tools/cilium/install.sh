14533127953e0c10bc230798150f149c2d7f4baf
Bao Dang <dang.hbaokody@gmail.com>
2025-06-13 10:54:02 +0700

    up Fri Jun 13 03:54:02 AM UTC 2025

--- k8s/essential-tools/cilium/install.sh
+++ k8s/essential-tools/cilium/install.sh
@@ -5,9 +5,6 @@ set -e
 SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-
 helm install cilium bitnami/cilium --version 3.0.2 \
 -f "$SCRIPT_DIR/values.yaml" \
 -n cilium --create-namespace
